37c47c4537fd963000cf97d0e57e263e12018b8c
admin.ayegiaya <admin.ayegiaya@ITS-DEPOT11>
2023-10-20 13:34:13 -0700

    Add project files.

---
+++ AutoSSDGUI/Form1.cs
@@ -0,0 +1,21 @@
+using System.Diagnostics;
+
+namespace AutoSSDGUI
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+    }
+}
